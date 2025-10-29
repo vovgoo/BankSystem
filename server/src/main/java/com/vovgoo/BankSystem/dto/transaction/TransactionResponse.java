24a7daac5851374916b1426ba4141707502e0ff5
Vova <vov.gonchar2017@yandex.ru>
2025-10-30 00:23:13 +0300

    refactor TransactionResponse

--- server/src/main/java/com/vovgoo/BankSystem/dto/transaction/TransactionResponse.java
+++ server/src/main/java/com/vovgoo/BankSystem/dto/transaction/TransactionResponse.java
@@ -1,19 +1,25 @@
 package com.vovgoo.BankSystem.dto.transaction;
 
 import com.vovgoo.BankSystem.dto.transaction.enums.TransactionStatus;
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TransactionResponse {
 
     private final TransactionStatus status;
-
     private final String message;
+    private final LocalDateTime timestamp;
 
-    @Builder.Default
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    public static TransactionResponse approve(String message) {
+        return new TransactionResponse(TransactionStatus.APPROVED, message, LocalDateTime.now());
+    }
+
+    public static TransactionResponse decline(String message) {
+        return new TransactionResponse(TransactionStatus.DECLINED, message, LocalDateTime.now());
+    }
 }
