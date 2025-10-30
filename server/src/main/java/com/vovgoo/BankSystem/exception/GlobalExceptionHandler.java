292fd8bdf075883fe5078689d125d1c1530e80aa
Vova <vov.gonchar2017@yandex.ru>
2025-10-30 03:21:39 +0300

    Create GlobalExceptionHandler

---
+++ server/src/main/java/com/vovgoo/BankSystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,81 @@
+package com.vovgoo.BankSystem.exception;
+
+import com.vovgoo.BankSystem.dto.transaction.TransactionResponse;
+import com.vovgoo.BankSystem.exception.account.AccountBalanceNotZeroException;
+import com.vovgoo.BankSystem.exception.account.InsufficientFundsException;
+import com.vovgoo.BankSystem.exception.account.SameAccountTransferException;
+import com.vovgoo.BankSystem.exception.client.ClientAlreadyExistsException;
+import com.vovgoo.BankSystem.exception.client.ClientHasActiveAccountsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ClientAlreadyExistsException.class)
+    public ResponseEntity<TransactionResponse> handleClientAlreadyExists(ClientAlreadyExistsException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(resp);
+    }
+
+    @ExceptionHandler(ClientHasActiveAccountsException.class)
+    public ResponseEntity<TransactionResponse> handleClientHasActiveAccounts(ClientHasActiveAccountsException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(resp);
+    }
+
+    @ExceptionHandler(AccountBalanceNotZeroException.class)
+    public ResponseEntity<TransactionResponse> handleAccountBalanceNotZero(AccountBalanceNotZeroException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(resp);
+    }
+
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<TransactionResponse> handleInsufficientFunds(InsufficientFundsException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(resp);
+    }
+
+    @ExceptionHandler(SameAccountTransferException.class)
+    public ResponseEntity<TransactionResponse> handleSameAccountTransfer(SameAccountTransferException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(resp);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<TransactionResponse> handleEntityNotFound(EntityNotFoundException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(resp);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<TransactionResponse> handleIllegalArgument(IllegalArgumentException ex) {
+        TransactionResponse resp = TransactionResponse.decline(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(resp);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<TransactionResponse> handleAll(Exception ex) {
+        TransactionResponse resp = TransactionResponse.decline("Внутренняя ошибка сервиса");
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(resp);
+    }
+}
