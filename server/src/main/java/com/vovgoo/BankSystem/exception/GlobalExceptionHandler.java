a694ebcdb70a28541c0869936ea021b5353bb122
Vova <vov.gonchar2017@yandex.ru>
2025-10-30 03:33:28 +0300

    add exception handler for jakarta validation

--- server/src/main/java/com/vovgoo/BankSystem/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/vovgoo/BankSystem/exception/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import com.vovgoo.BankSystem.exception.client.ClientHasActiveAccountsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -76,6 +78,22 @@ public class GlobalExceptionHandler {
         TransactionResponse resp = TransactionResponse.decline("Внутренняя ошибка сервиса");
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(resp);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<TransactionResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        StringBuilder errors = new StringBuilder();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.append(error.getField())
+                    .append(": ")
+                    .append(error.getDefaultMessage())
+                    .append("; ");
+        }
+
+        TransactionResponse resp = TransactionResponse.decline(errors.toString());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(resp);
     }
 }
