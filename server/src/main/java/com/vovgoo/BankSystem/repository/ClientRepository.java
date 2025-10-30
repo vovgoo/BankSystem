c73ae3465680de43d1e686f826294e47b3e9a7ce
Vova <vov.gonchar2017@yandex.ru>
2025-10-30 22:29:49 +0300

    refactor ClientRepository, create cusom query, which help avoid n + 1 problem

--- server/src/main/java/com/vovgoo/BankSystem/repository/ClientRepository.java
+++ server/src/main/java/com/vovgoo/BankSystem/repository/ClientRepository.java
@@ -1,15 +1,25 @@
 package com.vovgoo.BankSystem.repository;
 
+import com.vovgoo.BankSystem.dto.client.response.ClientResponse;
 import com.vovgoo.BankSystem.entity.Client;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, UUID> {
 
-    Page<Client> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable);
+    @Query("""
+        SELECT c.id AS id, c.lastName AS lastName, c.phone AS phone, COALESCE(SUM(a.balance), 0) AS totalBalance
+        FROM Client c
+        LEFT JOIN c.accounts a
+        WHERE (:lastName IS NULL OR LOWER(c.lastName) LIKE LOWER(CONCAT('%', :lastName, '%')))
+        GROUP BY c.id
+    """)
+    Page<ClientResponse> findClientSummaries(@Param("lastName") String lastName, Pageable pageable);
 
     boolean existsByPhone(String phone);
 }
