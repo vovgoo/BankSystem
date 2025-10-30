f828c785b7b525a027882caeb6dcfe05fe9cf719
Vova <vov.gonchar2017@yandex.ru>
2025-10-31 02:46:20 +0300

    set profile in test environment

--- server/src/test/java/com/vovgoo/BankSystem/BankSystemApplicationTests.java
+++ server/src/test/java/com/vovgoo/BankSystem/BankSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.vovgoo.BankSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BankSystemApplicationTests {
 
 	@Test
