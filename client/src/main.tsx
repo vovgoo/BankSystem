d864337cc3282216993fb02c0609ec25559d513a
Vova <vov.gonchar2017@yandex.ru>
2025-11-03 03:11:51 +0300

    refactor main file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider, Toaster } from "@components";
 import './index.css'
-import App from './App.tsx'
-import { Provider } from '@components/ui/provider';
+import App from "./App"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
+      <Toaster/>
       <App />
     </Provider>
   </StrictMode>,
